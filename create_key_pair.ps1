93330aa4f68fd85ab630d7c635d3b2f3c126d5a1
Peadar Grant <grantp@dkit.ie>
2022-10-17 15:14:13 +0100

    create key pair script

--- create_key_pair.ps1
+++ create_key_pair.ps1
@@ -3,17 +3,18 @@
 
 $KeyName="MAIN_KEY"
 $KeyFile="~/.ssh/id_rsa"
+$PublicKeyFile="$($KeyFile).pub"
 
 Write-Host "deleting key $KeyName if exists ... " -NoNewline
 aws ec2 delete-key-pair --key-name $KeyName
 Write-Host "ok" -ForegroundColor Green
 
-Write-Host "creating key pair on AWS ... " -NoNewline
-$Key=(aws ec2 create-key-pair --key-name $KeyName | ConvertFrom-Json)
+Write-Host "creating local key pair ... " -NoNewline
+ssh-keygen -trsa -b2048 -mpem  
 Write-Host "ok" -ForegroundColor Green
 
-Write-Host "saving private key file to $KeyFile ... " -NoNewline
-$Key.KeyMaterial | Out-File $KeyFile
+Write-Host "importing key pair to AWS ... " -NoNewline
+aws ec2 import-key-pair --key-name $KeyName --public-key-material file://$PublicKeyFile
 Write-Host "ok" -ForegroundColor Green
 
 Write-Host "created SSH key pair on AWS" -ForegroundColor Green
